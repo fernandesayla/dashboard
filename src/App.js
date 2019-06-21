e4ea8036a8534c94d0af9ef78a6f0172c8c4283b
aylafernandes <fernandes.ayla@gmail.com>
2019-06-21 19:42:52 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
-
+import AppNavBar from './components/AppNavBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import { blue, yellow } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+import responsiveFontSizes from '@material-ui/core/styles';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CssBaseline />
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <AppNavBar />
+        </ThemeProvider>
+      </div>
+    </Router>
   );
 }
+
+let theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: yellow
+  },
+  status: {
+    danger: 'orange'
+  }
+});
 
 export default App;
---
+++ src/components/AppNavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+function AppNavBar() {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h6">UCE</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default AppNavBar;
